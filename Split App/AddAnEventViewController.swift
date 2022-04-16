9d6abaf008df7a81404baf765b230bee34c11d68
snowyaya <yayingliang@gmail.com>
2022-04-16 00:25:56 -0400

    Fix buttons, add imageView

--- Split App/AddAnEventViewController.swift
+++ Split App/AddAnEventViewController.swift
@@ -6,11 +6,17 @@
 //
 
 import UIKit
+import AlamofireImage
+import Parse
 
-class AddAnEventViewController: UIViewController {
+class AddAnEventViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var eventNameField: UITextField!
     @IBOutlet weak var eventDate: UIDatePicker!
+    @IBOutlet weak var totalAmountField: UITextField!
+    @IBOutlet weak var peopleField: UITextField!
+    @IBOutlet weak var peopleStepper: UIStepper!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +32,34 @@ class AddAnEventViewController: UIViewController {
     }
     
     @IBAction func cancelAddAnEvent(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func peopleStepperPressed(_ sender: UIStepper) {
+        peopleField.text = Int(sender.value).description
+    }
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        // resize the image
+        let size = CGSize(width: 80, height: 80 )
+        let scaledImage = image.af.imageAspectScaled(toFill: size)
+        
+        imageView.image = scaledImage
         dismiss(animated: true, completion: nil)
     }
     
