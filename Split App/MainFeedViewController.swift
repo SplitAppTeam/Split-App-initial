750cceba809ca277f250ef10d24a3143494e1d9b
snowyaya <yayingliang@gmail.com>
2022-04-15 20:55:07 -0400

    Add image, update buttons

--- Split App/MainFeedViewController.swift
+++ Split App/MainFeedViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import Parse
 
 class MainFeedViewController: UIViewController {
 
+    var events = [PFObject]()
+    var selectedEvent: PFObject!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        var query = PFQuery(className: "Events")
+        query.includeKeys(["author", "name", "date"])
+        query.addDescendingOrder("createdAt")
+//        query.findObjectsInBackground { (events, error) in
+//            if events != nil {
+//                self.events = events!
+//                self.tableView.reloadData()
+//            }
+//        }
+    }
+    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        let event = events[section]
+//        // ?? means whatever is on the left is nil, then send [] to comments
+//        let name = (event["name"] as? [PFObject]) ?? []
+//        return events.count + 2
+//    }
     
     
     /*
