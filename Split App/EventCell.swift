2ffb8f50bb34ec2bc68b79b69b76269fbd5b2265
snowyaya <yayingliang@gmail.com>
2022-04-16 01:41:55 -0400

    Added view events

---
+++ Split App/EventCell.swift
@@ -0,0 +1,23 @@
+//
+//  EventCell.swift
+//  Split App
+//
+//  Created by Yaying Liang on 4/16/22.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Split App/MainFeedViewController.swift
+++ Split App/MainFeedViewController.swift
@@ -9,9 +9,6 @@ import UIKit
 import Parse
 
 class MainFeedViewController: UIViewController {
-
-    var events = [PFObject]()
-    var selectedEvent: PFObject!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,24 +19,7 @@ class MainFeedViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        var query = PFQuery(className: "Events")
-        query.includeKeys(["author", "name", "date"])
-        query.addDescendingOrder("createdAt")
-//        query.findObjectsInBackground { (events, error) in
-//            if events != nil {
-//                self.events = events!
-//                self.tableView.reloadData()
-//            }
-//        }
     }
-    
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        let event = events[section]
-//        // ?? means whatever is on the left is nil, then send [] to comments
-//        let name = (event["name"] as? [PFObject]) ?? []
-//        return events.count + 2
-//    }
-    
     
     /*
     // MARK: - Navigation
