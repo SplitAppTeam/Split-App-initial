67e50d2d6a0d6562e1a99d1bd01a4767b2b34363
stellashen0911 <minjieshen0911@gmail.com>
2022-04-22 13:07:32 -0700

    Update Event cell view

---
+++ Split App/EventTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  EventTableViewCell.swift
+//  Split App
+//
+//  Created by StellaShen on 2022/4/22.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+
+    
+}
